19fb1c26a0f7178c71f6e6455bb5e9942f81f592
ubuvolt <ubuvolt@gmail.com>
2020-01-16 07:56:43 +0100

    add resources/ app/ routes/

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
+
+//main page
+Route::get('/', function () {
+    return view('emails/myTestMail');
+});
+
+//login & registration
+Route::get('login', 'AuthController@index');
+Route::post('post-login', 'AuthController@postLogin'); 
+Route::get('registration', 'AuthController@registration');
+Route::post('post-registration', 'AuthController@postRegistration'); 
+Route::get('dashboard', 'AuthController@dashboard'); 
+Route::get('logout', 'AuthController@logout');
+
+//email
+Route::get('send-mail', function () {
+   
+    $details = [
+        'title' => 'Mail from ItSolutionStuff.com',
+        'body' => 'This is for testing email using smtp'
+    ];
+   
+    \Mail::to('your_receiver_email@gmail.com')->send(new \App\Mail\MyTestMail($details));
+   
+    dd("Email is Sent.");
+});
+
+
+
